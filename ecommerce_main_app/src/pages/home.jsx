ea7342b6a5f3111a8e187c72928fab4431643061
braahuss <braahussein@mail.tau.ac.il>
2024-01-24 21:43:30 +0200

    backgroundImage

--- ecommerce_main_app/src/pages/home.jsx
+++ ecommerce_main_app/src/pages/home.jsx
@@ -1,33 +1,24 @@
 // Home.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import external CSS file
 
 function Home() {
     return (
-        <div>
-            <header style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '1em' }}>
-                <img src="your-logo.png" alt="Your Logo" style={{ width: '100px', height: 'auto', display: 'block', margin: '0 auto' }} />
-            </header>
-
-            <main style={{ textAlign: 'center', marginTop: '50px' }}>
-                <a href="Customer" className="option" style={optionStyle}>Customer</a>
-                <a href="Contracter" className="option" style={optionStyle}>Contracter</a>
-            </main>
+        <div className="container">
+            <div className="background">
+               
+                <Link to="/customer" className="option">
+                    Customer
+                </Link>
+                <Link to="/contractor" className="option">
+                    Contractor
+                </Link>
+                
+            </div>
             
         </div>
     );
-};
-
-const optionStyle = {
-    display: 'inline-block',
-    margin: '20px',
-    padding: '20px',
-    backgroundColor: '#fff',
-    borderRadius: '10px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    textDecoration: 'none',
-    color: '#333',
-    fontSize: '18px',
-    transition: 'background-color 0.3s',
-};
+}
 
 export default Home;

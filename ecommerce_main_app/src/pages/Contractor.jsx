c71b67bc4b98faaf8ac5bd7b47bb832c6ea864db
hussainmussa <103756223+hussainmussa@users.noreply.github.com>
2024-01-27 14:19:05 +0200

    adding 4 fields to firestore

--- ecommerce_main_app/src/pages/Contractor.jsx
+++ ecommerce_main_app/src/pages/Contractor.jsx
@@ -1,37 +1,59 @@
-import React, { useRef } from "react";
-import {firestore } from "../firebase";
-import {addDoc,collection} from "@firebase/firestore";
-
+import React, { useRef, useState } from "react";
+import { firestore } from "../firebase";
+import { addDoc, collection } from "@firebase/firestore";
 
 const Contracter = () => {
-    const messageRef=useRef();
-    const ref =collection(firestore,"phonenumbers");
+  const [inputFields, setInputFields] = useState([
+    { id: 1, label: "PhoneNumber", value: "" },
+    { id: 2, label: "Full Name", value: "" },
+    { id: 3, label: "Job Field", value: "" },
+    { id: 4, label: "City", value: "" }
+  ]);
 
+  const ref = collection(firestore, "Contractors");
 
-    const handleSave = async(e) => {
-        e.preventDefault();
-        console.log(messageRef.current.value);
-        
-        let data={
-            message:messageRef.current.value,
-        };
-        try{
-            addDoc(ref,data)
-        }
-        catch{
-            console.log(e);
-        }   
-    };
-    
-    return ( 
+  const handleSave = async (e) => {
+    e.preventDefault();
+
+    let data = {};
+
+    inputFields.forEach((field) => {
+      data[field.label.toLowerCase()] = field.value;
+    });
+
+    try {
+      await addDoc(ref, data);
+      // Clear input fields after successful save
+      setInputFields(inputFields.map((field) => ({ ...field, value: "" })));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e, id) => {
+    const newInputFields = inputFields.map((field) =>
+      field.id === id ? { ...field, value: e.target.value } : field
+    );
+    setInputFields(newInputFields);
+  };
+
+  return (
     <div>
-        <form onSubmit={handleSave}>
-            <label> Enter your number    </label>
-            <input type ="text"  ref={messageRef}/>
-            <button type="submit">Save now</button>
-        </form>
+      <form onSubmit={handleSave}>
+        {inputFields.map((field) => (
+          <div key={field.id}>
+            <label> Enter {field.label} </label>
+            <input
+              type="text"
+              value={field.value}
+              onChange={(e) => handleChange(e, field.id)}
+            />
+          </div>
+        ))}
+        <button type="submit">Save now</button>
+      </form>
     </div>
-);
-}
+  );
+};
 
 export default Contracter;

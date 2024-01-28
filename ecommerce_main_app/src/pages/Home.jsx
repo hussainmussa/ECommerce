cbfefd40849e5de3cdc24b4e7fbb8dd3de4a9f2b
hussainmussa <103756223+hussainmussa@users.noreply.github.com>
2024-01-28 22:00:24 +0200

    working on the UI of the app

--- ecommerce_main_app/src/pages/Home.jsx
+++ ecommerce_main_app/src/pages/Home.jsx
@@ -2,34 +2,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'; // Import external CSS file
+import glow from '../images/glow.png';
 
 function Home() {
     return (
-        <div className="container">
-            <div className="background">
-                <h1>Welcome to Our Platform</h1>
+        <>
+            <h1 className="welcome-text">Welcome</h1>
+            <div className="combined-container">
                 <div className="options-container">
+                <h2 className="choice-text">What brings you here today:</h2>
                     <Link to="/customer" className="option">
                         <div className="option-content">
                             <span className="icon">👤</span>
                             <span className="label">Customer</span>
+                            <span className="icon">👤</span>
                         </div>
                     </Link>
                     <Link to="/contractor" className="option">
                         <div className="option-content">
                             <span className="icon">🛠️</span>
                             <span className="label">Contractor</span>
-                        </div>
-                    </Link>
-                    <Link to="/showdata" className="option">
-                        <div className="option-content">
-                            <span className="icon">📊</span>
-                            <span className="label">Data</span>
+                            <span className="icon">🛠️</span>
                         </div>
                     </Link>
                 </div>
+
             </div>
-        </div>
+
+            <h2>                
+                <div className="contact-links">
+                    <a href="/contact">Contact Us | Rate us</a>
+                </div>
+            </h2>
+
+            <img src={glow} alt="Description of Image" className="fixed-bottom-right" />
+            <img src={glow} alt="Description of Image" className="fixed-top-left" />
+        </>
+        
     );
 }
 

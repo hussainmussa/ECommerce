a8f9f89df9170c9c48b8c7f209a85c96776061ed
braahuss <braahussein@mail.tau.ac.il>
2024-03-17 02:33:33 +0200

    bug fix

--- ecommerce_main_app/src/pages/Home.jsx
+++ ecommerce_main_app/src/pages/Home.jsx
@@ -1,12 +1,13 @@
+// Home.jsx
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Home.css"; // Import external CSS file
-import { getAuth, signOut ,onAuthStateChanged} from "firebase/auth";
-
+import { getAuth,signOut, onAuthStateChanged } from "firebase/auth";
 
 function Home() {
-
   const auth = getAuth();
   const [phoneNumber, setPhoneNumber] = useState(null);
+
   useEffect(() => {
     // Listen for changes in authentication state
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -23,23 +24,7 @@ function Home() {
     // Clean up subscription on unmount
     return () => unsubscribe();
   }, [auth]);
-  
 
-  
-
-  const Button = ({ text, emoji, linkTo }) => {
-    return (
-      <button
-        className="rectangle"
-        onClick={() => linkTo && window.location.assign(linkTo)}
-      >
-        {emoji && <span className="user">{emoji}</span>}
-        <span className="sign-up">{text}</span>
-      </button>
-    );
-  };
-  
-  // Function to handle sign out
   const handleSignOut = () => {
     signOut(auth) // Call the signOut function
       .then(() => {
@@ -52,25 +37,33 @@ function Home() {
         console.error("Error signing out:", error);
       });
   };
+  
+  const Button = ({ text, emoji, linkTo }) => {
+    return (
+      <button
+        className="rectangle"
+        onClick={() => linkTo && window.location.assign(linkTo)}
+      >
+        {emoji && <span className="user">{emoji}</span>}
+        <span className="sign-up">{text}</span>
+      </button>
+    );
+  };
 
   return (
     <div className="main-container">
-     
-      <span className="welcome">Welcome</span>
+      <span className="welcome">Welcome {phoneNumber} </span>
       <span className="to-our-platform">To Our Platform</span>
-
-      <span className="please-sign-up">Phone Number: {phoneNumber} </span>
       <span className="please-sign-up">Please sign up </span>
       <Button
         text="Sign up"
         emoji={<div className="arrow-right" />}
         linkTo="/SignUp"
       />
-      
-
       <button className="sign-out-button" onClick={handleSignOut}>
         <span className="sign-out">Sign out</span>
       </button>
+      
       <span className="to-access-data">To access our data</span>
       <Button text="Contractor" emoji="🛠️" linkTo="/contractor" />
       <Button text="Data" emoji="📊" linkTo="/showdata" />
@@ -79,3 +72,5 @@ function Home() {
 }
 
 export default Home;
+
+//      <Button text="Customer" emoji="👤" linkTo="/customer" />

a3686d54d2d99a022ecce2851669d8e27050e2b2
braahuss <braahussein@mail.tau.ac.il>
2024-03-18 22:58:06 +0200

    Update Home.jsx

--- ecommerce_main_app/src/pages/Home.jsx
+++ ecommerce_main_app/src/pages/Home.jsx
@@ -1,41 +1,32 @@
-// Home.jsx
 import React, { useEffect, useState } from "react";
-import "./Home.css"; // Import external CSS file
-import { getAuth,signOut, onAuthStateChanged } from "firebase/auth";
-import BottomBar from "./BottomBar"; 
-import { useNavigate } from 'react-router-dom';
+import "./Home.css";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import BottomBar from "./BottomBar";
+import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 function Home() {
   const auth = getAuth();
-  const [phoneNumber, setPhoneNumber] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
   
   useEffect(() => {
-    // Listen for changes in authentication state
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in.
-        // Get user's phone number if available
-        setPhoneNumber(user.phoneNumber);
+        console.log("User is signed in:", user.phoneNumber);
       } else {
-        // No user is signed in.
-        setPhoneNumber(null);
+        console.log("No user is signed in.");
       }
     });
-
-    // Clean up subscription on unmount
     return () => unsubscribe();
   }, [auth]);
 
   const handleSignOut = () => {
-    signOut(auth) // Call the signOut function
+    signOut(auth)
       .then(() => {
-        // Sign-out successful.
         console.log("Sign-out successful");
         window.location.href = '/PhoneAuth';
       })
       .catch((error) => {
-        // An error happened.
         console.error("Error signing out:", error);
       });
   };
@@ -55,65 +46,53 @@ function Home() {
   const navigate = useNavigate();
 
   const buttonsContent = [
-    { text: "Electrician"},
-    { text: "Plumber"},
-    { text: "Mechanic"},
-    { text: "Marketing"},
-    { text: "Construction"},
-    { text: "Gardener"},
-    { text: "House Cleaner"},
-    { text: "Painter"},
-    { text: "Roofer"},
-    { text: "Locksmith"}
+    "Electrician", "Plumber", "Mechanic", "Marketing", "Construction",
+    "Gardener", "House Cleaner", "Painter", "Roofer", "Locksmith"
   ];
 
   return (
-    <div className="main-container">
-      
-      <button className="sign-out-button" onClick={handleSignOut}>
-        <span className="sign-out">Sign out</span>
-      </button>
+    <div className="BG-container">
+      <div className="main-container">
 
-      {/*<span className="welcome">Welcome {phoneNumber} </span>*/}
-      <span className="text-container">Welcome To Our Platform</span>
-      
-      <div className="title-search-container">
-      <input
-          type="text"
-          placeholder=" 🔍 Type to search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          
-        />
+        <span className="text-container">How can we help you?</span>
+
+        <button className="sign-out-button" onClick={handleSignOut}>
+          <span className="sign-out">Sign out</span>
+        </button>
+
+        <div className="title-search-container">
+          <div className="search-input-container">
+            <input
+              type="text"
+              placeholder=" Type to search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="search-icon-button" onClick={() => navigate("/ShowData", { state: { searchTerm } })}>
+              <FaSearch />
+            </button>
+          </div>
         </div>
-        <Button text="" emoji="🔍" />
-    
-      <div className="help-section">
-        <div className="line"></div>
-        <span className="help-text">Popular searches</span>
-        <div className="line"></div>
+
+        <div className="help-section">
+          <div className="line"></div>
+          <span className="help-text">Popular searches</span>
+          <div className="line"></div>
+        </div>
+
+        <div className="scroll-container">
+          {buttonsContent.map((button, index) => (
+            <button
+              key={index}
+              className="scroll-button"
+              onClick={() => navigate("/showdata", { state: { searchTerm: button } })}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
+
       </div>
-
-      {/*
-      <span className="to-access-data">To access our data</span>
-      <Button text="Contractor" emoji="🛠️" linkTo="/contractor" />
-      <Button text="Data" emoji="📊" linkTo="/showdata" />
-
-      */}
-
-<div className="scroll-container">
-  {buttonsContent.map((button, index) => (
-    <button 
-      key={index} 
-      className="scroll-button"
-      onClick={() => navigate('/showdata', { state: { searchTerm: button.text } })}
-    >
-      {button.text}
-    </button>
-  ))}
-</div>
-
-
       <BottomBar />
     </div>
 

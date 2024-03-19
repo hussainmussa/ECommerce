01ec7c7711a8ddc0f82c09d222d58246f58aaf11
hussainmussa <hussainmussa31@gmail.com>
2024-03-19 17:30:01 +0200

    adding a new welcome page

---
+++ ecommerce_main_app/src/pages/Welcome.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Welcome.css';
+import logo from '../images/HandyManLogo.jpg';
+import { useNavigate } from 'react-router-dom';
+
+const WelcomePage = () => {
+
+    const navigate = useNavigate();
+
+    const handleGetStartedClick = () => {
+        navigate('/PhoneAuth'); 
+      };
+
+
+  return (
+    <div className="welcome-container">
+      <div className="image-container">
+      <img src={logo} alt="Image" className="image-container" />
+      </div>
+      <div className="welcome-text">
+        <h1>Welcome!</h1>
+        <p>Get easy access to local services</p>
+      </div>
+      <button className="get-started-btn" onClick={handleGetStartedClick}>Get Started</button>
+    </div>
+  );
+};
+
+export default WelcomePage;

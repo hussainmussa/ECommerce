27cd387b5ee6fe1c3afef1b70378dc12a5d38a39
hussainmussa <hussainmussa31@gmail.com>
2024-03-20 01:09:02 +0200

    card UI

--- ecommerce_main_app/src/components/Card.jsx
+++ ecommerce_main_app/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import "./Card.css"; // Import external CSS file
 import React from "react";
 import myImage from "../images/HandyManBG.jpg";
+import star from "../images/star.png";
 
 const Card = ({ item, handleClick }) => {
   const fields = [
@@ -8,21 +9,24 @@ const Card = ({ item, handleClick }) => {
     { label: "Job", key: "jobfield" },
   ];
 
+
+  const randomFloat = Math.round((Math.random() * 4 + 1) * 10) / 10;
   return (
     <div key={item.id} className="data-card" onClick={() => handleClick(item)}>
+      
       <div className="card-content">
-        
-        <div>
-          <div className="data-value name">{item.fullname}</div>
-          {fields.map(({ label, key }) => (
-            <div style={{ display: "flex", alignItems: "center" }} key={key}>
-              <strong style={{ marginRight: "10px" }}>{label}:</strong>
-              <div className="data-value">{item[key]}</div>
-            </div>
-          ))}
-        </div>
         <img src={myImage} alt="icon" className="data-image" />
+        <div style={{ marginLeft: '50px' , width : '100px'}}>
+          <div className="data-value name">{item.fullname}</div>
+          <div style={{ marginTop: '-10px'}}>{item.city}</div>
+          <div style={{fontWeight:'bold' , marginTop: '23px'}}>{item.jobfield}</div>     
+        </div>
+        <div style={{ marginLeft : '50px'}}>
+          <div >{randomFloat}</div>   
+          <img src={star}  style={{ width: '40px' , height: '40px'}} />
+        </div>
       </div>
+
     </div>
   );
 };
